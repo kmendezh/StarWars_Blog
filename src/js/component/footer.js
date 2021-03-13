aa31642dc7b1db18350af29c70911ada8edf32e5
kmendezh <kendallmh2@hotmail.com>
2021-03-13 05:13:03 +0000

    Initial submit

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
+import "/workspace/react-hello-webapp/src/styles/footer.css";
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
+	<footer className="footerPage row">
+		<div className="col-2 flc">
+			<div className="row"> Fan Art from Daniel Ocock </div>{" "}
+			<div className="row"> TM © Lucasfilm Ltd. All Rights Reserved </div>
+		</div>
+		<div className="col-10">
+			<img
+				className="footerImage"
+				src="https://nerdist.com/wp-content/uploads/2017/03/Boba-Fett-Fan-Art-Daniel-Ocock-Featured-03102017.jpg"
+			/>
+		</div>
 	</footer>
 );
--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,16 +1,38 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "/workspace/react-hello-webapp/src/styles/navbar.css";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
+		<nav
+			style={{
+				backgroundColor: "black",
+				height: "120px"
+			}}
+			className="navbar navbar-light mb-3">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<img src="https://1000logos.net/wp-content/uploads/2017/06/Star-Wars-symbol.jpg" className="swImage" />
 			</Link>
 			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
+				<div className="ddicon dropdown">
+					<button
+						className="btn dropdown-toggle ddtext"
+						type="button"
+						id="dropdownMenu1"
+						data-toggle="dropdown"
+						aria-haspopup="true"
+						aria-expanded="false">
+						Favorites
+					</button>
+					<div className="dropdown-menu" aria-labelledby="dropdownMenu1">
+						<a className="dropdown-item" href="#!">
+							Action
+						</a>
+						<a className="dropdown-item" href="#!">
+							Another action
+						</a>
+					</div>
+				</div>
 			</div>
 		</nav>
 	);
